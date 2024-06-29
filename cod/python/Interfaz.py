b53da314db9a9e28a2415dd9f1f594730b6a51fe
fabianbrenesth <fabianbrenes0@gmail.com>
2024-06-29 14:01:41 -0600

    feat: se agrega la documentación de la Interfaz

--- cod/python/Interfaz.py
+++ cod/python/Interfaz.py
@@ -13,12 +13,23 @@ import subprocess
 
 class MainWindow(QWidget):
     def __init__(self):
+        """
+        Inicializa la ventana principal, configura el título, el tamaño y los elementos de la interfaz.
+        
+        Parameters
+        --------------
+        No tiene parámetros.
+        
+        Returns
+        -------------
+        No devuelve nada.
+        """
         super().__init__()
 
         self.setWindowTitle("Juegos")
         self.setFixedSize(500, 400)
         
-        # Set background image
+        # Para agregar la imágen de fondo
         palette = QPalette()
         bg_image = QPixmap("Graphics/background1.jpeg").scaled(self.size(), Qt.IgnoreAspectRatio)
         palette.setBrush(QPalette.Background, QBrush(bg_image))
@@ -26,14 +37,14 @@ class MainWindow(QWidget):
 
         layout = QVBoxLayout()
 
-        # Title label
+        # Títulos
         title_label = QLabel("Seleccione un juego:")
         title_label.setFont(QFont('Helvetica', 20, QFont.Bold))
         title_label.setStyleSheet("color: white; background-color: rgba(0, 0, 0, 0.6); padding: 10px; border-radius: 5px;")
         title_label.setAlignment(Qt.AlignCenter)
         layout.addWidget(title_label, alignment=Qt.AlignCenter)
 
-        # Buttons
+        # Botones
         btn_2048 = QPushButton("Jugar 2048")
         btn_2048.setFont(QFont('Helvetica', 14))
         btn_2048.setStyleSheet("background-color: rgba(0, 0, 0, 0.6); color: white; padding: 10px; border-radius: 5px;")
@@ -49,15 +60,48 @@ class MainWindow(QWidget):
         self.setLayout(layout)
 
     def run_2048_game(self):
+        """
+        Ejecuta el juego 2048.
+
+        Parameters
+        --------------
+        No tiene parámetros.
+        
+        Returns
+        -------------
+        No devuelve nada.
+        """
         self.close()
         subprocess.run([sys.executable, '_2048_main.py'])
 
     def run_snake_game(self):
+        """
+        Ejecuta el juego de la serpiente.
+
+        Parameters
+        --------------
+        No tiene parámetros.
+        
+        Returns
+        -------------
+        No devuelve nada.
+        """
         self.close()
         subprocess.run([sys.executable, 'Serpiente_main.py'])
         
 
 def main():
+    """
+    Función principal para ejecutar la aplicación de la interfaz de selección de juegos.
+
+    Parameters
+    --------------
+    No tiene parámetros.
+    
+    Returns
+    -------------
+    No devuelve nada.
+    """
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()

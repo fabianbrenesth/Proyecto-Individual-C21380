eb1adaee273b6e571775915537fbfd10aeb185e8
fabianbrenesth <fabianbrenes0@gmail.com>
2024-06-29 13:40:27 -0600

    fix: se corrige la indentación del script Serpiente_Fruta

--- cod/python/Serpiente_Fruta.py
+++ cod/python/Serpiente_Fruta.py
@@ -2,13 +2,14 @@
 """
 Created on Thu Jun 27 21:58:06 2024
 
-@author: Fabian
+Autor: Fabian
 """
-import pygame,random
+import pygame
+import random
 from pygame.math import Vector2
 
 class FRUIT:
-	def __init__(self, cell_number):
+    def __init__(self, cell_number):
         """
         Inicializa la fruta en una posición aleatoria.
 
@@ -20,9 +21,9 @@ class FRUIT:
         -------------
         No devuelve nada.
         """
-		self.randomize(cell_number)
+        self.randomize(cell_number)
 
-	def draw_fruit(self, cell_size, screen, apple):
+    def draw_fruit(self, cell_size, screen, apple):
         """
         Dibuja la fruta en la pantalla.
 
@@ -36,11 +37,11 @@ class FRUIT:
         -------------
         No devuelve nada.
         """
-		fruit_rect = pygame.Rect(int(self.pos.x * cell_size),int(self.pos.y * cell_size),cell_size,cell_size)
-		screen.blit(apple,fruit_rect)
-		#pygame.draw.rect(screen,(126,166,114),fruit_rect)
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size, cell_size)
+        screen.blit(apple, fruit_rect)
+        # pygame.draw.rect(screen,(126,166,114),fruit_rect)
 
-	def randomize(self, cell_number):
+    def randomize(self, cell_number):
         """
         Ubica la fruta en una posición aleatoria en la cuadrícula.
 
@@ -52,6 +53,6 @@ class FRUIT:
         -------------
         No devuelve nada.
         """
-		self.x = random.randint(0,cell_number - 1)
-		self.y = random.randint(0,cell_number - 1)
-		self.pos = Vector2(self.x,self.y)
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+        self.pos = Vector2(self.x, self.y)
